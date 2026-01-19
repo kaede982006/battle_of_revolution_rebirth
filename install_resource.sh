00edc992c00358efd689a1372e75b1d01100362c
kaede982006 <kaede982006@gmail.com>
2026-01-19 21:11:16 +0900

    20250119

--- install_resource.sh
+++ install_resource.sh
@@ -4,7 +4,7 @@ chmod +x ./yt-dlp
 ./yt-dlp -x --audio-format wav -o "res/bg01.wav" "https://youtu.be/xSjnN39Qhmc?si=h6uX36a7Tnss8pe7"
 ./yt-dlp -x --audio-format wav -o "res/bg02.wav" "https://youtu.be/YaDxGpzSVKU?si=uLfhk-sHFGBS57_X"
 ./yt-dlp -x --audio-format wav -o "res/bg03.wav" "https://youtu.be/5yYQIa-4rLE?si=Li1f1YtYCuwCJ3Zp"
-./yt-dlp -x --audio-format wav -o "res/fail.wav" "https://www.youtube.com/watch?v=e5tEoIrXK6o"
+./yt-dlp -x --audio-format wav -o "res/failure.wav" "https://www.youtube.com/watch?v=e5tEoIrXK6o"
 ./yt-dlp -x --audio-format wav -o "res/victory.wav" "https://youtu.be/itgSd5JIIBs?si=krUy8Afv2v6eOrvo"
 ./yt-dlp -x --audio-format wav -o "res/intro.wav" "https://www.youtube.com/watch?v=xJB3aRd-q74"
 rm ./yt-dlp
