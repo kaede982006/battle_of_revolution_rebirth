d220bd1e63a85403eebf00cc00b3c7360506e84d
kaede982006 <kaede982006@gmail.com>
2026-01-25 22:10:21 +0900

    2026-01-25 22:10:21

--- install_resource.sh
+++ install_resource.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
-wget https://github.com/yt-dlp/yt-dlp/releases/download/2025.12.08/yt-dlp
+
+package_array=( "git" "make" "ffmpeg" "build-essential" )
+file_array=( "res/bg01.wav" "res/bg02.wav" "res/bg03.wav" "res/failure.wav" "res/victory.wav" "res/intro.wav" )
+link_array=( "https://youtu.be/xSjnN39Qhmc?si=h6uX36a7Tnss8pe7" "https://youtu.be/YaDxGpzSVKU?si=uLfhk-sHFGBS57_X" "https://youtu.be/5yYQIa-4rLE?si=Li1f1YtYCuwCJ3Zp" \
+"https://www.youtube.com/watch?v=e5tEoIrXK6o" "https://youtu.be/itgSd5JIIBs?si=krUy8Afv2v6eOrvo" "https://www.youtube.com/watch?v=xJB3aRd-q74" )
+not_ok_array=()
+
+for p in ${package_array[@]}; do
+    package_query_value="$(dpkg-query -W --showformat='${db:Status-Status}' $p 2>&1)"
+    if [ $package_query_value == "installed" ]; then
+        echo "[$p -> ok]" 
+    else
+        echo "[$p -> not ok]"
+        not_ok_array+=("$p")
+    fi
+done
+if [ ${#not_ok_array[@]} -eq 0 ]; then
+    echo "[All packages are ok]"
+else
+    echo "[ERROR: Install packages following: ${not_ok_array[@]}]"
+    exit
+fi
+
+if [ ${#file_array[@]} -ne ${#link_array[@]} ]; then
+    echo "[ERROR: Internal error]"
+    exit
+fi
+array_num=${#file_array[@]}
+git clone https://github.com/yt-dlp/yt-dlp.git
+make -C yt-dlp
+cp yt-dlp/yt-dlp temp-yt-dlp
 chmod +x ./yt-dlp
-./yt-dlp -x --audio-format wav -o "res/bg01.wav" "https://youtu.be/xSjnN39Qhmc?si=h6uX36a7Tnss8pe7"
-./yt-dlp -x --audio-format wav -o "res/bg02.wav" "https://youtu.be/YaDxGpzSVKU?si=uLfhk-sHFGBS57_X"
-./yt-dlp -x --audio-format wav -o "res/bg03.wav" "https://youtu.be/5yYQIa-4rLE?si=Li1f1YtYCuwCJ3Zp"
-./yt-dlp -x --audio-format wav -o "res/failure.wav" "https://www.youtube.com/watch?v=e5tEoIrXK6o"
-./yt-dlp -x --audio-format wav -o "res/victory.wav" "https://youtu.be/itgSd5JIIBs?si=krUy8Afv2v6eOrvo"
-./yt-dlp -x --audio-format wav -o "res/intro.wav" "https://www.youtube.com/watch?v=xJB3aRd-q74"
-rm ./yt-dlp
+for ((i=0;i<array_num;i++)); do
+    ./temp-yt-dlp -x --audio-format wav \
+    -o "${file_array[$i]}" "${link_array[$i]}"
+done
+rm ./temp-yt-dlp
+rm -rf yt-dlp
+
+echo "[Done]"
+exit
